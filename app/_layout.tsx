1f03d043d7e7f2e9f1eb4148a3cce518d623233c
abdullah <abdullah@192.168.1.4>
2024-09-30 08:49:22 -0700

    fixing starter

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,25 +10,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useColorScheme } from 'nativewind';
 const queryClient = new QueryClient()
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const currentlanguage = getLocale()
+
 
   return (
-    <QueryClientProvider client={queryClient}>
-    <LoadingProvider>
-    <ThemeProvider value={colorScheme.colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
 
 
         <Stack >
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="onboarding/index" options={{ headerShown: false }} />
+
 
         </Stack>
-        <PortalHost/>
-    </ThemeProvider>
-    </LoadingProvider>
-    </QueryClientProvider>
+    
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,65 +1,9 @@
-import { useFonts } from "expo-font";
-import { Redirect, SplashScreen } from "expo-router";
-import { ONBOARDINGSTORGAGEKEY, storage } from "~/utils/storage";
-import AnimatedSplashScreen from "./animatedSplash";
-import useSplashHook from "~/hooks/splash/useSplashHook";
-import { useEffect, useState } from "react";
-import { initializeLanguage } from "~/utils/localization/helpers";
-import { ActivityIndicator, View } from "react-native";
-import { Colors } from "~/constants/Colors";
-import { useLoading } from "~/context/LoadingContext";
+import { View, Text } from 'react-native';
 
-SplashScreen.preventAutoHideAsync()
-
-const Index = () => {
-  const [showAnimatedSplash, setShowAnimatedSplash] = useState(false);
-  const [animationFinished, setAnimationFinished] = useState(false);
-  const UserSeeOnboarding = storage.getBoolean(ONBOARDINGSTORGAGEKEY)
-  const { setIsLoading} = useLoading()
-  const ready = useSplashHook()
-  const [loaded] = useFonts({
-    ElMessiriBold: require('../assets/fonts/El_Messiri/ElMessiri-Bold.ttf'),
-    ElMessiriMedium: require('../assets/fonts/El_Messiri/ElMessiri-Medium.ttf'),
-    ElMessiriRegular: require('../assets/fonts/El_Messiri/ElMessiri-Regular.ttf'),
-    ElMessiriSemiBold: require('../assets/fonts/El_Messiri/ElMessiri-SemiBold.ttf'),
-  });
-
-  useEffect(() => {
-    SplashScreen.hideAsync()
-    if (ready && loaded) {
-        setShowAnimatedSplash(true);
-        setAnimationFinished(true);
-        setIsLoading(true)
-
-    }
-  }, [ready, loaded])
-
-  const handleAnimationFinish = () => {
-    setAnimationFinished(false);
-    console.log('handle finxish animated')
-  };
-  
-    if (!ready && !loaded) {
-    return (
-      <AnimatedSplashScreen/>
-    );
-  }
-
-  if (showAnimatedSplash && !animationFinished) {
-    return <AnimatedSplashScreen onAnimationFinish={handleAnimationFinish} />;
-  }
-
-  if (animationFinished) {
-    if (!UserSeeOnboarding) {
-      console.log('redirecting to onboarding', UserSeeOnboarding)
-      return <Redirect href="/onboarding" />;
-    } else {
-      console.log('redirecting to tabs', UserSeeOnboarding)
-      return <Redirect href="/(tabs)" />;
-    }
-  }
-
- return <AnimatedSplashScreen/>
-};
-
-export default Index;
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+}

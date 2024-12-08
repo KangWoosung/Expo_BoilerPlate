d9c7dd3a1607f098e1b3f019bbe195a5fe66ac19
KangWoosung <53413020+KangWoosung@users.noreply.github.com>
2024-12-08 17:27:06 +0900

    Init Commit

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,9 @@ import { View, Text } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text className="bg-red-200 p-8">Hello, World! Template!!!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} className="gap-4">
+      <Text className="rounded-full bg-red-200 p-8">Hello, World! Template!!!</Text>
+      <Text className="rounded-full bg-red-200 p-8">One two three!!!</Text>
     </View>
   );
 }
